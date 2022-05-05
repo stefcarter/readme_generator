f4adcc3169e5d22b9ca9a77f8231b67511b18d98
Steffan Carter <xisled09@gmail.com>
2022-05-05 18:09:57 -0400

    More foundation code

--- index.js
+++ index.js
@@ -5,6 +5,52 @@ const fs = require('fs');
 inquirer
     .prompt([
         {
-            type: ''
-        }
+            type: 'input',
+            message: 'What is your title?',
+            name: 'description',
+          },
+          {
+            type: 'input',
+            message: 'What is your password?',
+            name: 'table of contents',
+          },
+          {
+            type: 'input',
+            message: 'What is your password?',
+            name: 'table of contents',
+          },
+          {
+            type: 'input',
+            message: 'Re-enter password to confirm:',
+            name: 'installation',
+          },
+          {
+            type: 'input',
+            message: 'What is your user name?',
+            name: 'usage',
+          },
+          {
+            type: 'input',
+            message: 'What is your password?',
+            name: 'license',
+          },
+          {
+            type: 'input',
+            message: 'Re-enter password to confirm:',
+            name: 'contributing',
+          },
+          {
+            type: 'input',
+            message: 'What is your password?',
+            name: 'tests',
+          },
+          {
+            type: 'input',
+            message: 'Re-enter password to confirm:',
+            name: 'questions',
+          },
     ])
+
+    fs.readFile('data.csv', 'utf8', (error, data) =>
+        error ? console.error(error) : console.log(data)
+    );
