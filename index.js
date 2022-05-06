9e931e99cd22cad5cbb7a1470279c2106e782f2a
Steffan Carter <xisled09@gmail.com>
2022-05-05 20:13:12 -0400

    base for generator done

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-const requirer = requier('requirer')
+const inquirer = require('inquirer');
 
 const fs = require('fs');
 
 inquirer
     .prompt([
         {
-            type: 'input',
-            message: 'What is your title?',
-            name: 'title',
-          },
-          {
-            type: 'input',
-            message: 'Any licensing',
-            name: 'license',
-          },
-          {
-            type: 'input',
-            message: 'What are your contents?',
-            name: 'contents',
-          },
-          {
-            type: 'input',
-            message: 'What is the description of your site?',
-            name: 'description',
-          },
+        type: 'input',
+        message: 'What is your title?',
+        name: 'title',
+        },
+        {
+        type: 'input',
+        message: 'Any licensing',
+        name: 'license',
+        },
+        {
+        type: 'input',
+        message: 'What are your contents?',
+        name: 'contents',
+        },
+        {
+        type: 'input',
+        message: 'What is the description of your site?',
+        name: 'description',
+        },
           
-          {
-            type: 'input',
-            message: 'What type of installation?',
-            name: 'installation',
-          },
-          {
-            type: 'input',
-            message: 'What will this site be used for?',
-            name: 'usage',
-          },
+        {
+        type: 'input',
+        message: 'What type of installation?',
+        name: 'installation',
+        },
+        {
+        type: 'input',
+        message: 'What will this site be used for?',
+        name: 'usage',
+        },
           
-          {
-            type: 'input',
-            message: '',
-            name: 'contributing',
-          },
-          {
-            type: 'input',
-            message: 'What is your password?',
-            name: 'tests',
-          },
-          {
-            type: 'input',
-            message: 'Re-enter password to confirm:',
-            name: 'questions',
-          },
+        {
+        type: 'input',
+        message: 'Want to contribute?',
+        name: 'contributing',
+        },
+        {
+        type: 'input',
+        message: 'What tests are done?',
+        name: 'tests',
+        },
+        {
+        type: 'input',
+        message: 'Got questions?',
+        name: 'questions',
+        },
     ])
     .then(answers => {
+        console.log(answers)
+        let thingtosave =
         `
         #Title
             ${answers.title}
@@ -78,8 +80,8 @@ inquirer
         ##Questions
             ${answers.questions}
         `
-    })
     
-    fs.writeFile("myanswers.md", thingtosave, (err)=>{
+    fs.writeFile("answers.md", thingtosave, (err)=>{
         err ? console.log(err) : console.log("File written!");
     })
+})
